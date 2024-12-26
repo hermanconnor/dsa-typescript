86d949d53f5d0cf672eb722d4f177118980df9cc
herman <hermanconnor11@gmail.com>
2024-12-26 16:44:08 -0500

    Add push, pop, and peek methods

--- src/stack/Stack.ts
+++ src/stack/Stack.ts
@@ -6,6 +6,32 @@ class Stack<T> {
   }
 
   /**
+   * Pushes a new item onto the top of the stack.
+   * @param item The item to be pushed onto the stack.
+   */
+  public push(item: T): void {
+    this.items.push(item);
+  }
+
+  /**
+   * Removes and returns the item at the top of the stack
+   * @returns The item removed from the top of the stack,
+   *          or undefined if the stack is empty.
+   */
+  public pop(): T | undefined {
+    return this.items.pop();
+  }
+
+  /**
+   * Returns the item at the top of the stack without removing it.
+   * @returns The item at the top of the stack,
+   *          or undefined if the stack is empty.
+   */
+  public peek(): T | undefined {
+    return this.items[this.items.length - 1];
+  }
+
+  /**
    * Returns the number of items currently in the stack.
    * @returns The number of items in the stack.
    */
