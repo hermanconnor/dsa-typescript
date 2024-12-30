7a7a22e73271271323110e53038f78673c46eecb
herman <hermanconnor11@gmail.com>
2024-12-29 20:14:53 -0500

    Edit singly linked list

--- src/data-structures/singly-linked-list/SinglyLinkedList.ts
+++ src/data-structures/singly-linked-list/SinglyLinkedList.ts
@@ -11,30 +11,28 @@ class SinglyLinkedList<T> {
     this.length = 0;
   }
 
-  unshift(value: T): void {
+  insertFirst(value: T): void {
     const newNode = new ListNode(value);
-
-    newNode.next = this.head;
-    this.head = newNode;
-
-    if (!this.tail) {
-      this.tail = newNode;
-    }
-
-    this.length++;
-  }
-
-  push(value: T): void {
-    const newNode = new ListNode(value);
-
-    if (this.tail) {
-      this.tail.next = newNode;
-    }
-
-    this.tail = newNode;
 
     if (!this.head) {
       this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+  }
+
+  insertLast(value: T): void {
+    const newNode = new ListNode(value);
+
+    if (!this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
 
     this.length++;
