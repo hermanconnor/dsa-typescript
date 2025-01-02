9b017a39fd5233f084c7fb9f680dccb7f02dc672
herman <hermanconnor11@gmail.com>
2025-01-02 18:46:31 -0500

    Add tests for push and pop

--- tests/LinkedList.test.ts
+++ tests/LinkedList.test.ts
@@ -12,4 +12,25 @@ describe('Singly Linked List', () => {
     expect(list.isEmpty).toBe(true);
     expect(list.size).toBe(0);
   });
+
+  it('push should add a node to the end of the list', () => {
+    list.unshift(10);
+    list.push(20);
+
+    expect(list.size).toBe(2);
+    expect(list.getTail()?.value).toBe(20);
+    expect(list.getHead()?.value).toBe(10);
+  });
+
+  it('pop should remove and return the last node in the list', () => {
+    list.push(10);
+    list.push(20);
+    list.push(30);
+
+    const poppedNode = list.pop();
+
+    expect(poppedNode?.value).toBe(30);
+    expect(list.size).toBe(2);
+    expect(list.getTail()?.value).toBe(20);
+  });
 });
