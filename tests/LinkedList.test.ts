d5846f95be577c345be3fb74fe34c0c0739201b6
herman <hermanconnor11@gmail.com>
2025-01-02 19:16:48 -0500

    Add tests for unshift, shift, and insertNodeAt

--- tests/LinkedList.test.ts
+++ tests/LinkedList.test.ts
@@ -22,6 +22,10 @@ describe('Singly Linked List', () => {
     expect(list.getHead()?.value).toBe(10);
   });
 
+  it('pop should return null if the list is empty', () => {
+    expect(list.pop()).toBe(null);
+  });
+
   it('pop should remove and return the last node in the list', () => {
     list.push(10);
     list.push(20);
@@ -32,5 +36,47 @@ describe('Singly Linked List', () => {
     expect(poppedNode?.value).toBe(30);
     expect(list.size).toBe(2);
     expect(list.getTail()?.value).toBe(20);
+  });
+
+  it('unshift should add a node to the beginning of the list', () => {
+    list.push(20);
+    list.push(30);
+    list.unshift(10);
+
+    expect(list.size).toBe(3);
+    expect(list.getHead()?.value).toBe(10);
+    expect(list.getTail()?.value).toBe(30);
+  });
+
+  it('shift should return null if the list is empty', () => {
+    expect(list.shift()).toBe(null);
+  });
+
+  it('shift should remove and return the first node in the list', () => {
+    list.push(10);
+    list.push(20);
+
+    const removedNode = list.shift();
+
+    expect(removedNode?.value).toBe(10);
+    expect(list.size).toBe(1);
+    expect(list.getHead()?.value).toBe(20);
+  });
+
+  it('insertNodeAt should not insert a node at an invalid index', () => {
+    const inserted = list.insertNodeAt(1, 100);
+
+    expect(inserted).toBe(false);
+  });
+
+  it('insertNodeAt should insert a node at a specific index', () => {
+    list.push(10);
+    list.push(30);
+
+    const inserted = list.insertNodeAt(1, 20);
+
+    expect(inserted).toBe(true);
+    expect(list.size).toBe(3);
+    expect(list.getNodeAt(1)?.value).toBe(20);
   });
 });
