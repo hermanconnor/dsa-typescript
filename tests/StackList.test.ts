247ee42f56af825d18a32c738121002100597773
herman <hermanconnor11@gmail.com>
2025-01-06 16:16:57 -0500

    Add tests for push and pop to stacklist

--- tests/StackList.test.ts
+++ tests/StackList.test.ts
@@ -12,4 +12,28 @@ describe('StackList', () => {
     expect(stack.isEmpty).toBe(true);
     expect(stack.size).toBe(0);
   });
+
+  it('push should add an item onto the stack', () => {
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+
+    expect(stack.size).toBe(3);
+    expect(stack.peek()).toBe(30);
+  });
+
+  it('pop should throw an error if the stack is empty', () => {
+    expect(() => stack.pop()).toThrow(/empty/i);
+  });
+
+  it('pop should remove and return the top item in the stack', () => {
+    stack.push(10);
+    stack.push(20);
+    stack.push(30);
+
+    const poppedItem = stack.pop();
+
+    expect(poppedItem).toBe(30);
+    expect(stack.size).toBe(2);
+  });
 });
