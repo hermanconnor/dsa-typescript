26e29bd8057943c2a775c428b79535b5de383ac7
herman <hermanconnor11@gmail.com>
2025-01-20 23:53:06 -0500

    Add enqueue, getTail, isEmpty, size, methods to queue

--- src/data-structures/queue/Queue.ts
+++ src/data-structures/queue/Queue.ts
@@ -10,6 +10,39 @@ class Queue<T> {
     this.tail = null;
     this.length = 0;
   }
+
+  enqueue(value: T): void {
+    const newNode = new QueNode(value);
+
+    if (this.isEmpty()) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      if (this.tail) {
+        this.tail.next = newNode;
+      }
+
+      this.tail = newNode;
+    }
+
+    this.length++;
+  }
+
+  getHead() {
+    return this.head;
+  }
+
+  getTail() {
+    return this.tail;
+  }
+
+  size(): number {
+    return this.length;
+  }
+
+  isEmpty(): boolean {
+    return this.length === 0;
+  }
 }
 
 export default Queue;
--- tests/Queue.test.ts
+++ tests/Queue.test.ts
@@ -8,4 +8,52 @@ describe('Queue', () => {
   beforeEach(() => {
     queue = new Queue();
   });
+
+  it('queue should initially be empty', () => {
+    expect(queue.isEmpty()).toBe(true);
+    expect(queue.size()).toBe(0);
+  });
+
+  it('enqueue should add an item to the end of an empty queue correctly', () => {
+    queue.enqueue(10);
+
+    expect(queue.getHead()?.value).toBe(10);
+    expect(queue.getTail()?.value).toBe(10);
+    expect(queue.size()).toBe(1);
+  });
+
+  it('enqueue should add an item to the end of a non-empty queue correctly', () => {
+    queue.enqueue(10);
+    queue.enqueue(20);
+
+    expect(queue.getTail()?.value).toBe(20);
+    expect(queue.getHead()?.value).toBe(10);
+    expect(queue.size()).toBe(2);
+  });
+
+  it('enqueue should maintain FIFO order when multiple items are enqueued', () => {
+    queue.enqueue(10);
+    queue.enqueue(20);
+    queue.enqueue(30);
+
+    expect(queue.getHead()?.value).toBe(10);
+    expect(queue.getTail()?.value).toBe(30);
+    expect(queue.size()).toBe(3);
+  });
+
+  it('enqueue should increment the length after enqueueing', () => {
+    expect(queue.size()).toBe(0);
+    queue.enqueue(10);
+    expect(queue.size()).toBe(1);
+    queue.enqueue(20);
+    expect(queue.size()).toBe(2);
+  });
+
+  it('enqueue should update the previous tail node to point to the new node', () => {
+    queue.enqueue(10);
+    const prevTail = queue.getTail();
+    queue.enqueue(20);
+
+    expect(prevTail?.next?.value).toBe(20);
+  });
 });
