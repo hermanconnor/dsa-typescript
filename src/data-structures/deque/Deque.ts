b799ff24d5efd4ead260d28461f9947c2b30582d
herman <hermanconnor11@gmail.com>
2025-01-28 19:19:39 -0500

    Add peekFirst and addFirst methods to deque

--- src/data-structures/deque/Deque.ts
+++ src/data-structures/deque/Deque.ts
@@ -11,6 +11,29 @@ class Deque<T> {
     this.length = 0;
   }
 
+  addFirst(value: T): void {
+    const newNode = new DequeNode(value);
+
+    if (this.isEmpty()) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+
+      if (this.head) {
+        this.head.prev = newNode;
+      }
+
+      this.head = newNode;
+    }
+
+    this.length++;
+  }
+
+  peekFirst(): T | null {
+    return this.head?.value ?? null;
+  }
+
   size(): number {
     return this.length;
   }
--- tests/Deque.test.ts
+++ tests/Deque.test.ts
@@ -12,4 +12,41 @@ describe('Deque', () => {
     expect(deque.isEmpty()).toBe(true);
     expect(deque.size()).toBe(0);
   });
+
+  it('addFirst should add an item to the front of the deque', () => {
+    deque.addFirst(10);
+    deque.addFirst(20);
+
+    expect(deque.peekFirst()).toBe(20);
+    expect(deque.size()).toBe(2);
+  });
+
+  it('peekFirst should return null if there are no items in the deque', () => {
+    expect(deque.peekFirst()).toBeNull();
+  });
+
+  it('peekFirst should return the first element in the deque without removing it', () => {
+    deque.addFirst(10);
+    deque.addFirst(20);
+
+    expect(deque.peekFirst()).toBe(20);
+    expect(deque.size()).toBe(2);
+  });
+
+  it('size should correctly return the size of the deque', () => {
+    deque.addFirst(10);
+    deque.addFirst(20);
+
+    expect(deque.size()).toBe(2);
+  });
+
+  it('isEmpty should return true if the deque is empty', () => {
+    expect(deque.isEmpty()).toBe(true);
+  });
+
+  it('isEmpty should return false if the deque is not empty', () => {
+    deque.addFirst(10);
+
+    expect(deque.isEmpty()).toBe(false);
+  });
 });
