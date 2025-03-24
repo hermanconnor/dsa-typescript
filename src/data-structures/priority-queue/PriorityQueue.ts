6d13ceee0e4e4b18ffa1e01f50e3acd9260f1cfd
herman <hermanconnor11@gmail.com>
2025-03-23 21:45:55 -0400

    Finish priority queue class

--- src/data-structures/priority-queue/PriorityQueue.ts
+++ src/data-structures/priority-queue/PriorityQueue.ts
@@ -8,9 +8,21 @@ class PriorityQueue<T> {
     this.compare = compare;
   }
 
-  enqueue(element: T): void {}
+  enqueue(element: T): void {
+    this.heap.push(element);
+    this.heapifyUp(this.heap.length - 1);
+  }
 
-  dequeue(): T | undefined {}
+  dequeue(): T | undefined {
+    if (this.isEmpty()) return undefined;
+    if (this.heap.length === 1) return this.heap.pop();
+
+    const root = this.heap[0];
+    this.heap[0] = this.heap.pop()!;
+    this.heapifyDown(0);
+
+    return root;
+  }
 
   peek(): T | undefined {
     return this.heap[0];
@@ -22,6 +34,60 @@ class PriorityQueue<T> {
 
   size(): number {
     return this.heap.length;
+  }
+
+  private getParentIndex(index: number): number {
+    return Math.floor((index - 1) / 2);
+  }
+
+  private getLeftChildIndex(index: number): number {
+    return 2 * index + 1;
+  }
+
+  private getRightChildIndex(index: number): number {
+    return 2 * index + 2;
+  }
+
+  private swap(index1: number, index2: number): void {
+    [this.heap[index1], this.heap[index2]] = [
+      this.heap[index2],
+      this.heap[index1],
+    ];
+  }
+
+  private heapifyUp(index: number): void {
+    while (
+      index > 0 &&
+      this.compare(this.heap[index], this.heap[this.getParentIndex(index)]) < 0
+    ) {
+      this.swap(index, this.getParentIndex(index));
+      index = this.getParentIndex(index);
+    }
+  }
+
+  private heapifyDown(index: number): void {
+    const leftChildIndex = this.getLeftChildIndex(index);
+    const rightChildIndex = this.getRightChildIndex(index);
+    let smallest = index;
+
+    if (
+      leftChildIndex < this.heap.length &&
+      this.compare(this.heap[leftChildIndex], this.heap[smallest]) < 0
+    ) {
+      smallest = leftChildIndex;
+    }
+
+    if (
+      rightChildIndex < this.heap.length &&
+      this.compare(this.heap[rightChildIndex], this.heap[smallest]) < 0
+    ) {
+      smallest = rightChildIndex;
+    }
+
+    if (smallest !== index) {
+      this.swap(index, smallest);
+      this.heapifyDown(smallest);
+    }
   }
 }
 
