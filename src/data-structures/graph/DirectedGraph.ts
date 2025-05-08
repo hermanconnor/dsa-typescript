51a4887ec73fe0e811b8da8ee694874b84cf70e4
herman <hermanconnor11@gmail.com>
2025-05-07 22:32:16 -0400

    Refactor directed graph and add support for optional weights

--- src/data-structures/graph/DirectedGraph.ts
+++ src/data-structures/graph/DirectedGraph.ts
@@ -1,137 +1,66 @@
 import Queue from '../queue/Queue';
 
 class DirectedGraph<T> {
-  adjList: Map<T, T[]>;
+  private adjList: Map<T, Map<T, number | undefined>>;
 
   constructor() {
     this.adjList = new Map();
   }
 
-  addVertex(vertex: T): boolean {
+  public addVertex(vertex: T): void {
     if (!this.adjList.has(vertex)) {
-      this.adjList.set(vertex, []);
-      return true;
+      this.adjList.set(vertex, new Map());
     }
-
-    return false;
   }
 
-  addEdge(vertex1: T, vertex2: T): boolean {
-    if (vertex1 === vertex2) return false;
-
-    this.addVertex(vertex1);
-    this.addVertex(vertex2);
-
-    const vertex1Array = this.adjList.get(vertex1)!;
-    const edgeAdded = !vertex1Array.includes(vertex2);
-
-    if (edgeAdded) {
-      vertex1Array.push(vertex2);
+  public addEdge(from: T, to: T, weight?: number): void {
+    if (!this.adjList.has(from)) {
+      this.addVertex(from);
     }
 
-    return edgeAdded;
+    if (!this.adjList.has(to)) {
+      this.addVertex(to);
+    }
+
+    this.adjList.get(from)!.set(to, weight);
   }
 
-  removeVertex(vertex: T): boolean {
-    if (!this.adjList.has(vertex)) return false;
+  public removeVertex(vertex: T): void {
+    if (!this.adjList.has(vertex)) return;
+
+    for (const [, neighbors] of this.adjList) {
+      if (neighbors.has(vertex)) {
+        neighbors.delete(vertex);
+      }
+    }
 
     this.adjList.delete(vertex);
-
-    this.adjList.forEach((edges, key) => {
-      this.adjList.set(
-        key,
-        edges.filter((neighbor) => neighbor !== vertex),
-      );
-    });
-
-    return true;
   }
 
-  removeEdge(vertex1: T, vertex2: T): boolean {
-    if (!this.adjList.has(vertex1)) return false;
+  public removeEdge(from: T, to: T): void {
+    if (!this.adjList.has(from)) return;
 
-    const vertex1Edges = this.adjList.get(vertex1)!;
+    const neighbors = this.adjList.get(from)!;
 
-    if (!vertex1Edges.includes(vertex2)) return false;
-
-    this.adjList.set(
-      vertex1,
-      vertex1Edges.filter((neighbor) => neighbor !== vertex2),
-    );
-
-    return true;
-  }
-
-  hasEdge(vertex1: T, vertex2: T): boolean {
-    if (!this.adjList.has(vertex1) || !this.adjList.has(vertex2)) return false;
-
-    return this.adjList.get(vertex1)!.includes(vertex2);
-  }
-
-  dfs(start: T): void {
-    if (!this.adjList.has(start)) {
-      console.error(`Start vertex: ${start}, not found.`);
-      return;
-    }
-
-    const visited = new Set<T>();
-
-    const dfsHelper = (vertex: T) => {
-      if (!visited.has(vertex)) {
-        console.log(vertex); // Visit the vertex
-
-        visited.add(vertex);
-
-        const neighbors = this.adjList.get(vertex);
-
-        if (neighbors) {
-          for (const neighbor of neighbors) {
-            if (!visited.has(neighbor)) {
-              dfsHelper(neighbor);
-            }
-          }
-        }
-      }
-    };
-
-    dfsHelper(start);
-  }
-
-  bfs(start: T): void {
-    if (!this.adjList.has(start)) {
-      console.error(`Start vertex: ${start}, not found.`);
-      return;
-    }
-
-    const visited = new Set<T>();
-    const queue = new Queue<T>();
-
-    queue.enqueue(start);
-    visited.add(start);
-
-    while (!queue.isEmpty()) {
-      const vertex = queue.dequeue();
-      if (!vertex) continue;
-
-      console.log(vertex); // Visit the vertex
-
-      const neighbors = this.adjList.get(vertex);
-
-      if (neighbors) {
-        for (const neighbor of neighbors) {
-          if (!visited.has(neighbor)) {
-            queue.enqueue(neighbor);
-            visited.add(neighbor);
-          }
-        }
-      }
+    if (neighbors.has(to)) {
+      neighbors.delete(to);
     }
   }
 
-  printGraph(): void {
-    this.adjList.forEach((edges, vertex) => {
-      console.log(`${vertex}: ${edges.join(', ')}`);
-    });
+  public hasVertex(vertex: T): boolean {
+    return this.adjList.has(vertex);
+  }
+
+  public hasEdge(from: T, to: T): boolean {
+    const neighbors = this.adjList.get(from);
+
+    return !!neighbors && neighbors.has(to);
+  }
+
+  public getNeighbors(vertex: T): T[] {
+    const neighbors = this.adjList.get(vertex);
+
+    return neighbors ? Array.from(neighbors.keys()) : [];
   }
 }
 
